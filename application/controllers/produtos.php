9391177588c32a10e8f83a59fd4b11fedf579423
Gustavo Rizzo Albuquerque <grizzo.albu@gmail.com>
2019-10-14 22:26:55 -0300

    acessando o bd mysql

--- application/controllers/produtos.php
+++ application/controllers/produtos.php
@@ -3,11 +3,10 @@
     class Produtos extends CI_Controller{
         public function index(){
 
-            // Criando lista de produtos
-            $produtos = array();            
-            $bola = array( "nome" => "Bola de futbol", "descricao" => "bola de futbol assinada pelo Zico", "preco" => 300);
-            $hd = array("nome" => "HD Externo usado", "marca" => "Mega HD 300 teras", "preco" => 400);
-            array_push( $produtos ,$bola , $hd);
+            // Busaca produtos no banco
+            $this->load->database();
+            $this->load->model("produtos_model");
+            $produtos = $this->produtos_model->buscaTodos();
             
             // Enviado dados para view
             $dados = array("produtos" => $produtos);
---
+++ application/models/produtos_model.php
@@ -0,0 +1,7 @@
+<?php
+    class Produtos_model extends CI_Model {
+        public function buscaTodos() {
+            return $this->db->get("produtos")->result_array();
+        }
+    }
+?>
