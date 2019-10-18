4707e423025408f8d6badf59837567a1938278c9
Gustavo Rizzo Albuquerque <grizzo.albu@gmail.com>
2019-10-18 01:08:21 -0300

    instalando bootstrap

--- application/views/frutas/index.php
+++ application/views/frutas/index.php
@@ -4,52 +4,53 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>fruta e fruto</title>
-
-        
+        <title>fruta e fruto</title>        
         <link rel="stylesheet" type="text/css" href="<?= base_url("src/style.css") ?>">
 
-        <!-- <link rel="stylesheet" type="text/css" href="src/style.css">
--->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
     </head>
 
     <body>
-    <title>fruto & fruta</title>
+        <title>fruto & fruta</title>
 
-    <h1>Reaproveite melhor os alimentos!</h1>
-    <h1>Economize e ganhe em saúde!</h1>
+        <h1>Reaproveite melhor os alimentos!</h1>
+        <h1>Economize e ganhe em saúde!</h1>
 
-    <h2>Receitas para economizar e ganhar saúde</h2>
-    <h2>Nossas receitas ajudam você a aproveitar melhor os alimentos, economizar, ganhar tempo e praticidade</h2>
+        <h2>Receitas para economizar e ganhar saúde</h2>
+        <h2>Nossas receitas ajudam você a aproveitar melhor os alimentos, economizar, ganhar tempo e praticidade</h2>
 
-    <img src=<?= base_url("src/img/receita-abacate.jpg")?> alt="tigela com salada de abacate, vista superior">
-    <h3>Tigela de abacate</h3>
-    <p>Receita refrescante e cheia de vitaminas para o seu café da manhã!</p>
+        <img src=<?= base_url("src/img/receita-abacate.jpg")?> alt="tigela com salada de abacate, vista superior">
+        <h3>Tigela de abacate</h3>
+        <p>Receita refrescante e cheia de vitaminas para o seu café da manhã!</p>
 
-    <img src=<?= base_url("src/img/receita-kiwi.jpg")?> alt="tigela de mingau com kiwi, vista superior">
-    <h3>Salada de kiwi</h3>
-    <p>Receita refrescante e cheia de vitaminas para o seu café da manhã!</p>
+        <img src=<?= base_url("src/img/receita-kiwi.jpg")?> alt="tigela de mingau com kiwi, vista superior">
+        <h3>Salada de kiwi</h3>
+        <p>Receita refrescante e cheia de vitaminas para o seu café da manhã!</p>
 
-    <img src=<?= base_url("src/img/receita-mix.jpg")?> alt="prato com mix de vegetais e mão de pessoa adicionando azeite, vista superior">
-    <h3>Mix de vegetais</h3>
-    <p>Receita refrescante e cheia de vitaminas para o seu café da manhã!</p>
+        <img src=<?= base_url("src/img/receita-mix.jpg")?> alt="prato com mix de vegetais e mão de pessoa adicionando azeite, vista superior">
+        <h3>Mix de vegetais</h3>
+        <p>Receita refrescante e cheia de vitaminas para o seu café da manhã!</p>
 
-    <img src=<?= base_url("src/img/receita-pimentoes.jpg")?> alt="prato de salada de pimentões com alguns ingredientes ao lado, vista superior">
-    <h3>Pimentões à Juliana</h3>
-    <p>Receita refrescante e cheia de vitaminas para o seu café da manhã!</p>
+        <img src=<?= base_url("src/img/receita-pimentoes.jpg")?> alt="prato de salada de pimentões com alguns ingredientes ao lado, vista superior">
+        <h3>Pimentões à Juliana</h3>
+        <p>Receita refrescante e cheia de vitaminas para o seu café da manhã!</p>
 
-    <img src=<?= base_url("src/img/receita-oriental.jpg")?> alt="tigela com salada de vegetais estilo oriental, vista de cima">
-    <h3>Prato oriental</h3>
-    <p>Receita refrescante e cheia de vitaminas para o seu café da manhã!</p>
+        <img src=<?= base_url("src/img/receita-oriental.jpg")?> alt="tigela com salada de vegetais estilo oriental, vista de cima">
+        <h3>Prato oriental</h3>
+        <p>Receita refrescante e cheia de vitaminas para o seu café da manhã!</p>
 
-    <img src=<?= base_url("src/img/receita-beterrabas.jpg")?> alt="tigela com beterrabas e dentes de alho assados em primeiro plano, com ingredientes ao fundo">
-    <h3>Beterrabas assadas</h3>
-    <p>Receita refrescante e cheia de vitaminas para o seu café da manhã!
-    </p>
+        <img src=<?= base_url("src/img/receita-beterrabas.jpg")?> alt="tigela com beterrabas e dentes de alho assados em primeiro plano, com ingredientes ao fundo">
+        <h3>Beterrabas assadas</h3>
+        <p>Receita refrescante e cheia de vitaminas para o seu café da manhã!
+        </p>
 
-    <p>contato:<a href="mailto:email@frutafruto.com.br">email@frutafruto.com.br</a></p>
+        <p>contato:<a href="mailto:email@frutafruto.com.br">email@frutafruto.com.br</a></p>
 
-    <script src=""></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src=""></script>
 
     </body>
 </html>
